ec45b4fc4b7122fe7eaa91944f973c0a2162ad97
Segger <saintdane@gmail.com>
2015-08-28 00:05:12 -0400

    Fiend bugfix
    Was previously not giving proper fiend reference

--- fight.js
+++ fight.js
@@ -47,11 +47,10 @@ this.npc = function(self,target,player){
 	if (!player.fiend || player.fiend.hp<=0){						//Is fiend dead?
 		if (player.level < 10){player.fiend = fiends.slime();}
 		if (player.level >= 10){player.fiend = fiends.jackal();}
-	
-		fiend = player.fiend;
 		self.pm(player.nick,player.fiend.encounter);
 	}
 
+	fiend = player.fiend;
 
 	//Damage to enemy
 	var strikeChance = Math.random()+0.3;			 				//Player's hit percentage			
